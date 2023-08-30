7940bbc59467442b3d7917090fc2d10a4fa058e6
Ahmad Mudaafi <ahmad.mudaafi@constructor.io>
2023-08-30 23:18:42 +0800

    Update Stories

--- src/stories/hooks/UseCioClient.stories.ts
+++ src/stories/hooks/UseCioClient.stories.ts
@@ -1,7 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { DEMO_API_KEY } from '../../constants';
 
-import UseCioClientExample from './UseCioClientExample';
+import UseCioClientExample, { useCioClientExampleCode } from './UseCioClientExample';
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 const meta = {
@@ -9,9 +9,18 @@ const meta = {
   component: UseCioClientExample,
   parameters: {
     layout: 'centered',
+    docs: {
+      source: {
+        code: useCioClientExampleCode,
+        language: 'jsx',
+        format: true,
+        type: 'code',
+      },
+    },
   },
   // eslint-disable-next-line @cspell/spellchecker
   tags: ['autodocs'],
+
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
 } satisfies Meta<typeof UseCioClientExample>;
 
@@ -22,6 +31,5 @@ type Story = StoryObj<typeof meta>;
 export const Primary: Story = {
   args: {
     apiKey: DEMO_API_KEY,
-    searchQuery: 'Shirt',
   },
 };
--- src/stories/hooks/UseCioClientExample.tsx
+++ src/stories/hooks/UseCioClientExample.tsx
@@ -1,7 +1,6 @@
-import React, { useEffect, useMemo, useRef, useState } from 'react';
+import React, { useState } from 'react';
 import { DEMO_API_KEY } from '../../constants';
 import useCioClient from '../../hooks/useCioClient';
-import HookRerenderControls from './HookRerenderControls';
 
 /**
  * This interface will be rendered as a table in Storybook
@@ -13,38 +12,84 @@ interface UseCioClientExampleProps {
    * API Key used to requests results from
    */
   apiKey?: string;
-  /**
-   * Query to search for
-   */
-  searchQuery?: string;
 }
 
 /**
  * Primary UI component for user interaction
  * Default values here will be reflected in the interface table
  */
-export default function UseCioClientExample({ apiKey, searchQuery }: UseCioClientExampleProps) {
+export default function UseCioClientExample({ apiKey }: UseCioClientExampleProps) {
   const cioClient = useCioClient(apiKey || DEMO_API_KEY);
   const [results, setResults] = useState({});
+  const [searchQuery, setSearchQuery] = useState('');
 
-  // const results = useMemo(
-  //   () =>
-  //     cioClient.search.getSearchResults(searchQuery || 'shirts').then((res) => {
-  //       console.log(res);
-  //       return res.result_id;
-  //     }),
-  //   [searchQuery, cioClient],
-  // );
-  useEffect(() => {
-    cioClient.search.getSearchResults(searchQuery || 'shirts').then((res) => {
-      // setResults(res.response.results);
-    });
-  });
+  const onInputHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(e.target.value);
+  };
+
+  const runSearch = async () => {
+    const res = await cioClient.search.getSearchResults(searchQuery || '');
+    setResults(res.response.results);
+  };
+
   return (
-    <HookRerenderControls>
+    <>
       <h1>Search Results as JSON</h1>
-      <div>{JSON.stringify(apiKey || DEMO_API_KEY)}</div>
+      <input
+        type='text'
+        name='searchBox'
+        id='searchBox'
+        placeholder='Search query'
+        value={searchQuery}
+        onChange={onInputHandler}
+      />
       <div>{JSON.stringify(results)}</div>
-    </HookRerenderControls>
+      <div>
+        <button type='button' onClick={runSearch}>
+          Search
+        </button>
+      </div>
+    </>
   );
 }
+
+export const useCioClientExampleCode = `
+import React, { useState } from 'react';
+import useCioClient from '../../hooks/useCioClient';
+
+const apiKey = 'MY_API_KEY'
+
+function UseCioClientExample() {
+  const cioClient = useCioClient(apiKey);
+  const [results, setResults] = useState({});
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const onInputHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(e.target.value);
+  };
+
+  const runSearch = async () => {
+    const res = await cioClient.search.getSearchResults(searchQuery || '');
+    setResults(res.response.results);
+  };
+
+  return (
+    <>
+      <h1>Search Results as JSON</h1>
+      <input
+        type='text'
+        name='searchBox'
+        id='searchBox'
+        placeholder='Search query'
+        value={searchQuery}
+        onChange={onInputHandler}
+      />
+      <div>{JSON.stringify(results)}</div>
+      <div>
+        <button type='button' onClick={runSearch}>
+          Search
+        </button>
+      </div>
+    </>
+  );
+}`;
