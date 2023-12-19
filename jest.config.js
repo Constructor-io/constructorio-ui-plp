42a3226a62791d8e8ccd8a0102faf41708274dce
mocca102 <islaam.agamy@gmail.com>
2023-12-19 16:23:02 +0200

    SSR React tests for CioPlpContext

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+// jest.config.js
+module.exports = {
+  projects: [
+    {
+      displayName: 'client',
+      testEnvironment: 'jsdom',
+      // Todo: If we agree on a naming convention for test files, we can add .client.test.js to the testMatch array
+      // If we do this, we can remove the second regex that excludes server.test.js files
+      testMatch: ['**/**/*.test.js', '!**/?(*.)+(server.test).js'],
+    },
+    {
+      displayName: 'server',
+      testEnvironment: 'node',
+      testMatch: ['**/**/*.server.test.js'],
+    },
+  ]
+};
