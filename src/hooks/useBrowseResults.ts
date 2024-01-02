7a979d6fd35f1fab02c910baa2eb8cd66c707c33
Islam Moustafa <58053149+mocca102@users.noreply.github.com>
2024-01-02 17:00:11 +0200

    Tests for server side rendering useBrowseResults (#40)
    * Add server side rendering tests for useCioClient
    
    * Test server side rendering for useSearchResults
    
    * Tests for server side rendering useBrowseResults
    
    * Resolve eslint with jsx
    
    * change file extension from js to jsx for tests with jsx
    
    * update apiResponse mocks to apiSearchResponse and apiBrowseRespons
    
    * remove wrongly created file
    
    * check filterName and filterValue are defined for handleSubmit event

--- src/hooks/useBrowseResults.ts
+++ src/hooks/useBrowseResults.ts
@@ -32,16 +32,23 @@ export default function useBrowseResults(
   filterName: string,
   filterValue: string,
   configs: UseBrowseResultsConfig = {},
+  initialBrowseResponse?: PlpBrowseResponse,
 ): UseBrowseResultsReturn {
   const { cioClient, browseParams } = configs;
   const state = useCioPlpContext();
   const client = cioClient || state?.cioClient;
 
-  if (!client) {
+  if (!filterName || !filterValue) {
+    throw new Error('filterName and filterValue are required');
+  }
+  // Throw error if client is not provided and window is defined (i.e. not SSR)
+  if (!client && typeof window !== 'undefined') {
     throw new Error('CioClient required');
   }
 
-  const [browseResponse, setBrowseResponse] = useState<PlpBrowseResponse | null>(null);
+  const [browseResponse, setBrowseResponse] = useState<PlpBrowseResponse | null>(
+    initialBrowseResponse || null,
+  );
   const pagination = usePagination({
     initialPage: browseResponse?.rawResponse.request?.page,
     totalNumResults: browseResponse?.totalNumResults,
@@ -49,16 +56,18 @@ export default function useBrowseResults(
   });
 
   const handleSubmit = () => {
-    client.browse
-      .getBrowseResults(filterName, filterValue, {
-        ...browseParams,
-        page: pagination.currentPage || browseParams?.page,
-      })
-      .then((res) => setBrowseResponse(transformBrowseResponse(res)));
+    if (client && filterName && filterValue) {
+      client.browse
+        .getBrowseResults(filterName, filterValue, {
+          ...browseParams,
+          page: pagination.currentPage || browseParams?.page,
+        })
+        .then((res) => setBrowseResponse(transformBrowseResponse(res)));
+    }
   };
 
   useEffect(() => {
-    if (filterName && filterValue) {
+    if (client && filterName && filterValue) {
       client.browse
         .getBrowseResults(filterName, filterValue, {
           ...browseParams,
