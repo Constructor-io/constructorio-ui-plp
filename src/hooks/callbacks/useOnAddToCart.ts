e313ca34bdb84ffcd4fa7281a97e2194cc53208b
Hossam Hindawy <hossam.hindawy@hossam.hindawy-Q7M9F6QK6G>
2024-02-13 03:22:01 +0200

    csl-3018 - Track search term for conversion/clickthrough events

--- src/hooks/callbacks/useOnAddToCart.ts
+++ src/hooks/callbacks/useOnAddToCart.ts
@@ -4,25 +4,32 @@ import ConstructorIO from '@constructor-io/constructorio-client-javascript';
 import { Nullable } from '@constructor-io/constructorio-client-javascript/lib/types';
 
 import { Item } from '../../types';
+import useRequestConfigs from '../useRequestConfigs';
 
 export default function useOnAddToCart(
   cioClient: Nullable<ConstructorIO>,
   getPrice: (item: Item) => number,
   callback?: (event: React.MouseEvent, item: Item) => void,
 ) {
+  const requestConfigs = useRequestConfigs();
+
+  if (!requestConfigs) {
+    throw new Error('This component is meant to be used within the CioPlp provider.');
+  }
+
   return useCallback(
     (event: React.MouseEvent, item: Item) => {
       const { itemId, itemName, variationId } = item;
+      const { query, section } = requestConfigs;
       const revenue = getPrice(item);
 
-      // TODO: Obtain the search term, if it exists - CSL3018
       if (cioClient) {
-        cioClient.tracker.trackConversion(undefined, {
+        cioClient.tracker.trackConversion(query, {
           itemId,
           itemName,
           variationId,
           revenue,
-          section: 'Products',
+          section,
         });
       }
 
@@ -31,6 +38,6 @@ export default function useOnAddToCart(
       event.preventDefault();
       event.stopPropagation();
     },
-    [callback, getPrice, cioClient],
+    [callback, getPrice, cioClient, requestConfigs],
   );
 }
