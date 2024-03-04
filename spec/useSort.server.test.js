a481c681c4f284de3e852221f421fd561a184372
Hossam Hindawy <hossam.hindawy@hossam.hindawy-Q7M9F6QK6G>
2024-03-05 00:20:19 +0200

    csl-3158 add tests

---
+++ spec/useSort.server.test.js
@@ -0,0 +1,64 @@
+import '@testing-library/jest-dom';
+import useSort from '../src/hooks/useSort';
+import { transformSearchResponse, transformSortOptionsResponse } from '../src/utils/transformers';
+import mockSearchResponse from './local_examples/apiSearchResponse.json';
+import { renderHookServerSide, renderHookServerSideWithCioPlp } from './test-utils.server';
+import { DEMO_API_KEY } from '../src/constants';
+
+describe('Testing Hook on the server: useSort', () => {
+  beforeEach(() => {
+    // Mock console error to de-clutter the console for expected errors
+    const spy = jest.spyOn(console, 'error');
+    spy.mockImplementation(() => {});
+  });
+
+  afterEach(() => {
+    jest.restoreAllMocks(); // This will reset all mocks after each test
+  });
+
+  const searchResponse = transformSearchResponse(mockSearchResponse);
+  const responseSortOptions = transformSortOptionsResponse(searchResponse.sortOptions);
+
+  it('Should throw an error if called outside of PlpContext', () => {
+    expect(() => renderHookServerSide(() => useSort(searchResponse))).toThrow();
+  });
+
+  it('Should not break if window is undefined', async () => {
+    expect(() =>
+      renderHookServerSideWithCioPlp(() => useSort(searchResponse), {
+        apiKey: DEMO_API_KEY,
+      }),
+    ).not.toThrow();
+  });
+
+  it('Should return sortOptions array', async () => {
+    const {
+      result: { sortOptions },
+    } = renderHookServerSideWithCioPlp(() => useSort(searchResponse), {
+      apiKey: DEMO_API_KEY,
+    });
+
+    expect(sortOptions).toHaveLength(responseSortOptions.length);
+    expect(sortOptions).toEqual(responseSortOptions);
+  });
+
+  it('Should return null sort option', async () => {
+    const {
+      result: { selectedSort },
+    } = renderHookServerSideWithCioPlp(() => useSort(searchResponse), {
+      apiKey: DEMO_API_KEY,
+    });
+
+    expect(selectedSort).toBeNull();
+  });
+
+  it('Should return a function to change selected sort', async () => {
+    const {
+      result: { changeSelectedSort },
+    } = renderHookServerSideWithCioPlp(() => useSort(searchResponse), {
+      apiKey: DEMO_API_KEY,
+    });
+
+    expect(typeof changeSelectedSort).toBe('function');
+  });
+});
---
+++ spec/useSort.test.js
@@ -0,0 +1,96 @@
+import '@testing-library/jest-dom';
+import { renderHook, waitFor } from '@testing-library/react';
+import useSort from '../src/hooks/useSort';
+import { transformSearchResponse, transformSortOptionsResponse } from '../src/utils/transformers';
+import mockSearchResponse from './local_examples/apiSearchResponse.json';
+import { renderHookWithCioPlp } from './test-utils';
+
+describe('Testing Hook: useSort', () => {
+  let location;
+  const mockLocation = new URL('https://example.com');
+
+  beforeEach(() => {
+    // Mock console error to de-clutter the console for expected errors
+    const spy = jest.spyOn(console, 'error');
+    spy.mockImplementation(() => {});
+
+    location = window.location;
+    delete window.location;
+    window.location = mockLocation;
+    mockLocation.href = 'https://example.com/';
+  });
+
+  afterEach(() => {
+    window.location = location;
+    jest.restoreAllMocks(); // This will reset all mocks after each test
+  });
+
+  const searchResponse = transformSearchResponse(mockSearchResponse);
+  const responseSortOptions = transformSortOptionsResponse(searchResponse.sortOptions);
+
+  it('Should throw error if called outside of PlpContext', () => {
+    expect(() => renderHook(() => useSort())).toThrow();
+  });
+
+  it('Should return sortOptions array', async () => {
+    const { result } = renderHookWithCioPlp(() => useSort(searchResponse));
+
+    await waitFor(() => {
+      const {
+        current: { sortOptions },
+      } = result;
+
+      expect(sortOptions).toHaveLength(responseSortOptions.length);
+      expect(sortOptions).toEqual(responseSortOptions);
+    });
+  });
+
+  it('Should return the default sort option if none is already selected in request configs', async () => {
+    const { result } = renderHookWithCioPlp(() => useSort(searchResponse));
+
+    await waitFor(() => {
+      const {
+        current: { selectedSort },
+      } = result;
+
+      expect(selectedSort).toEqual(responseSortOptions[0]);
+    });
+  });
+
+  it('Should return pre-selected sort option', async () => {
+    const sortBy = 'item_name';
+    const sortOrder = 'ascending';
+    window.location.href = `https://www.example.com/group_id/test?sortBy=${sortBy}&sortOrder=${sortOrder}`;
+
+    const { result } = renderHookWithCioPlp(() => useSort(searchResponse));
+
+    await waitFor(() => {
+      const {
+        current: { selectedSort },
+      } = result;
+
+      expect(selectedSort.sortBy).toEqual(sortBy);
+      expect(selectedSort.sortOrder).toEqual(sortOrder);
+    });
+  });
+
+  it('Should change selected sort option correctly', async () => {
+    const { result } = renderHookWithCioPlp(() => useSort(searchResponse));
+
+    await waitFor(() => {
+      const {
+        current: { selectedSort, changeSelectedSort },
+      } = result;
+
+      changeSelectedSort({
+        sortBy: 'item_name',
+        sortOrder: 'descending',
+        displayName: 'Name Z-A',
+      });
+
+      expect(selectedSort.sortBy).toEqual('item_name');
+      expect(selectedSort.sortOrder).toEqual('descending');
+      expect(selectedSort.displayName).toEqual('Name Z-A');
+    });
+  });
+});
