d1ed75bdcf0a92a7727503729cac5096fb67c109
Hossam Hassan Hindawy <7ossam9063@gmail.com>
2024-05-02 17:34:45 +0300

    Export Sort component and useSort hook (#74)
    Co-authored-by: Hossam Hindawy <hossam.hindawy@hossam.hindawy-Q7M9F6QK6G>

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,13 @@ export { default as useCioPlp } from './hooks/useCioPlp';
 export { default as useSearchResults } from './hooks/useSearchResults';
 export { default as useBrowseResults } from './hooks/useBrowseResults';
 export { default as useCioClient } from './hooks/useCioClient';
+export { default as useSort } from './hooks/useSort';
 export * from './hooks/callbacks';
 
 // Components
 export { default as CioPlp } from './components/CioPlp';
 export { default as ProductCard, ProductCardProps } from './components/ProductCard';
+export { default as Sort } from './components/Sort/Sort';
 
 export { useCioPlpContext } from './hooks/useCioPlpContext';
 export * from './types';
