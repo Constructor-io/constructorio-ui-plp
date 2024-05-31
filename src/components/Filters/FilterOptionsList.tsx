99e2537e385787ba5aaa15742872e7349ce20c8b
Ahmad Mudaafi <ahmad.mudaafi@constructor.io>
2024-06-01 01:58:24 +0800

    CSS Updates - Options List

--- src/components/Filters/FilterOptionsList.tsx
+++ src/components/Filters/FilterOptionsList.tsx
@@ -45,17 +45,38 @@ export default function FilterOptionsList(props: FilterOptionsListProps) {
 
   return (
     <div className={`cio-collapsible-wrapper${!isCollapsed ? ' cio-collapsible-is-open' : ''}`}>
-      <ul className='cio-filter-multiple-options cio-collapsible-inner'>
+      <ul className='cio-filter-multiple-options-list cio-collapsible-inner'>
         {optionsToRender.map((option) => (
           <li className='cio-filter-multiple-option' key={option.value}>
-            <button onClick={() => onOptionSelect(option.value)} type='button'>
-              <span className='cio-filter-option-checkbox'>
-                {selectedOptionMap[option.value] ? '[x] ' : '[ ] '}
-              </span>
-              <span className='cio-filter-option-name'>{option.displayName}</span>
-              &nbsp;
-              <span className='cio-filter-option-count'>({option.count})</span>
-            </button>
+            <label htmlFor={option.value}>
+              <input
+                type='checkbox'
+                id={option.value}
+                value={`${option.displayName}`}
+                checked={selectedOptionMap[option.value]}
+                onClick={() => onOptionSelect(option.value)}
+              />
+              <div className='cio-checkbox'>
+                <svg
+                  width='10'
+                  height='8'
+                  viewBox='0 0 10 8'
+                  fill='none'
+                  xmlns='http://www.w3.org/2000/svg'
+                  className='cio-check'>
+                  <path
+                    d='M1 4L3.5 6.5L9 1'
+                    stroke='white'
+                    strokeWidth='1.7'
+                    strokeLinecap='round'
+                  />
+                </svg>
+              </div>
+              <div className='cio-filter-multiple-option-display'>
+                <span className='cio-filter-option-name'>{option.displayName}</span>
+                <span className='cio-filter-option-count'>{option.count}</span>
+              </div>
+            </label>
           </li>
         ))}
 
