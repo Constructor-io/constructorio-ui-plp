4cd39f686e508f9876ef836321136af9a9c7c4bd
Ahmad Mudaafi <ahmad.mudaafi@constructor.io>
2024-08-31 05:23:41 +0800

    Remove unexported hooks from prod storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,6 +1,20 @@
 import type { StorybookConfig } from "@storybook/react-vite";
+let storiesRegexPaths = [
+  "../src/stories/*.mdx", 
+  "../src/stories/**/components/**/*.mdx",
+  "../src/stories/**/components/**/*.stories.@(js|jsx|ts|tsx)",
+  "../src/stories/**/components/**/*.stories.@(js|jsx|ts|tsx)",
+  "../src/stories/**/hooks/*.mdx",
+  "../src/stories/**/hooks/UseCioPlp/*.mdx",
+  "../src/stories/**/hooks/UseCioPlp/*.stories.@(js|jsx|ts|tsx)",
+]
+
+if (process.env.NODE_ENV === 'dev') {
+  storiesRegexPaths = ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"]
+}
+
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: storiesRegexPaths,
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
