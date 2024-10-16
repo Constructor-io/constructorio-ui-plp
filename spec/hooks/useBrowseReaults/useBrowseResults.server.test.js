8cdeac55ce8ac524263276a5f3ecb4709c002f6a
mocca102 <islaam.agamy@gmail.com>
2024-10-16 17:59:41 +0300

    Update useBrowsResults hook and tests

--- spec/hooks/useBrowseReaults/useBrowseResults.server.test.js
+++ spec/hooks/useBrowseReaults/useBrowseResults.server.test.js
@@ -5,6 +5,26 @@ import mockBrowseResponse from '../../local_examples/apiBrowseResponse.json';
 import { transformBrowseResponse } from '../../../src/utils/transformers';
 
 describe('Testing Hook on the server: useBrowseResults with initial browse results', () => {
+  beforeEach(() => {
+    // Mock console error to de-clutter the console for expected errors
+    const spy = jest.spyOn(console, 'error');
+    spy.mockImplementation(() => {});
+  });
+
+  afterEach(() => {
+    jest.restoreAllMocks(); // This will reset all mocks after each test
+  });
+
+  it('Should throw an error if used outside <CioPlp />', async () => {
+    const initialSearchResponse = transformBrowseResponse(mockBrowseResponse);
+
+    expect(() =>
+      renderHookServerSideWithCioPlp(() => useBrowseResults({ initialSearchResponse }), {
+        apiKey: DEMO_API_KEY,
+      }),
+    ).not.toThrow();
+  });
+
   it('Should not break if cioClient is null', async () => {
     const initialBrowseResponse = transformBrowseResponse(mockBrowseResponse);
 
@@ -22,16 +42,17 @@ describe('Testing Hook on the server: useBrowseResults with initial browse resul
   });
 
   it('Should return a PlpBrowseResponse Object when provided initialBrowseResponse', async () => {
-    const initialBrowseResponse = transformBrowseResponse(mockBrowseResponse);
-
     const { result } = renderHookServerSideWithCioPlp(
-      () => useBrowseResults({ initialBrowseResponse }),
+      () => useBrowseResults({ initialBrowseResponse: mockBrowseResponse }),
       {
         apiKey: DEMO_API_KEY,
       },
     );
 
-    const { response, resultId, rawApiResponse } = result.browseResults;
+    const {
+      data: { response, rawApiResponse, resultId },
+    } = result;
+
     expect(resultId).not.toBeUndefined();
     expect(response.totalNumResults).not.toBeUndefined();
     expect(response.refinedContent).not.toBeUndefined();
@@ -44,6 +65,16 @@ describe('Testing Hook on the server: useBrowseResults with initial browse resul
 });
 
 describe('Testing Hook on the server: useBrowseResults with no initialBrowseResponse', () => {
+  beforeEach(() => {
+    // Mock console error to de-clutter the console for expected errors
+    const spy = jest.spyOn(console, 'error');
+    spy.mockImplementation(() => {});
+  });
+
+  afterEach(() => {
+    jest.restoreAllMocks(); // This will reset all mocks after each test
+  });
+
   it('Should not break if cioClient is null', async () => {
     expect(() =>
       renderHookServerSideWithCioPlp(() => useBrowseResults(), {
@@ -53,11 +84,12 @@ describe('Testing Hook on the server: useBrowseResults with no initialBrowseResp
   });
 
   it('Should return null when called with no initialBrowseResponse', async () => {
-    const { result } = renderHookServerSideWithCioPlp(() => useBrowseResults(), {
+    const {
+      result: { data },
+    } = renderHookServerSideWithCioPlp(() => useBrowseResults(), {
       apiKey: DEMO_API_KEY,
     });
 
-    const response = result.browseResults;
-    expect(response).toBeNull();
+    expect(data).toBeNull();
   });
 });
