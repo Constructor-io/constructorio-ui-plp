2aa7cb233c11cb9132f8a815872405c84c803627
Ahmad Mudaafi <ahmad.mudaafi@constructor.io>
2024-11-21 09:21:03 +0800

    Export the components, hooks, types

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,9 @@ export { default as useCioPlp } from './hooks/useCioPlp';
 export { default as useSearchResults } from './hooks/useSearchResults';
 export { default as useBrowseResults } from './hooks/useBrowseResults';
 export { default as useCioClient } from './hooks/useCioClient';
+export { default as useFilter } from './hooks/useFilter';
+export { default as useGroups } from './hooks/useGroups';
+export { default as useOptionsList } from './hooks/useOptionsList';
 export { default as useSort } from './hooks/useSort';
 export * from './hooks/callbacks';
 
@@ -14,6 +17,7 @@ export { default as ProductCard } from './components/ProductCard';
 export { default as Sort } from './components/Sort';
 export { default as Pagination } from './components/Pagination';
 export { default as Filters } from './components/Filters';
+export { default as Groups } from './components/Groups';
 export { default as ProductSwatch } from './components/ProductSwatch';
 export { default as Spinner } from './components/Spinner';
 
@@ -25,4 +29,6 @@ export type { ProductSwatchProps } from './components/ProductSwatch';
 export type { SortProps, SortWithRenderProps } from './components/Sort';
 export type { PaginationProps, PaginationWithRenderProps } from './components/Pagination';
 export type { FiltersProps, FiltersWithRenderProps } from './components/Filters';
+export type { GroupsProps, GroupsWithRenderProps } from './components/Groups';
+export type { UseOptionsListProps } from './hooks/useOptionsList';
 export * from './types';
