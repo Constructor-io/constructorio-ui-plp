81f880b373d21a4d7ba470a3b92785733f78776d
S M Adeel <smaa.link@gmail.com>
2025-07-18 17:34:23 +0500

    [CI-4670]- Add data-cnstrc-result-id data attribute to the results container

--- spec/components/CioPlpGrid/CioPlpGrid.server.test.jsx
+++ spec/components/CioPlpGrid/CioPlpGrid.server.test.jsx
@@ -87,5 +87,6 @@ describe('Testing Component on the server: CioPlpGrid', () => {
 
     expect(html).toContain('data-cnstrc-search');
     expect(html).toContain('data-cnstrc-num-results="357"');
+    expect(html).toContain('data-cnstrc-result-id="c7000c32-0399-4bae-8e23-fb67bd0ecb62"');
   });
 });
