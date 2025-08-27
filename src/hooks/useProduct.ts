a8ab1ff2bf357ea137582a3dd6e5e4402512d07a
Evan Yan <evan.yan@constructor.io>
2025-08-27 10:00:36 -0700

    Move isValidSalePrice logic

--- src/hooks/useProduct.ts
+++ src/hooks/useProduct.ts
@@ -1,7 +1,7 @@
 import useProductSwatch from './useProductSwatch';
 import { useCioPlpContext } from './useCioPlpContext';
 import { UseProductInfo } from '../types';
-import { tryCatchify } from '../utils';
+import { tryCatchify, isValidSalePrice } from '../utils';
 import {
   getPrice as defaultGetPrice,
   getSalePrice as defaultGetSalePrice,
@@ -24,10 +24,6 @@ const useProductInfo: UseProductInfo = ({ item }) => {
   const itemUrl = productSwatch?.selectedVariation?.url || item.url;
   const variationId = productSwatch?.selectedVariation?.variationId;
   const { itemId } = item;
-
-  function isValidSalePrice(salePrice: number, usualPrice: number) {
-    return salePrice && usualPrice && salePrice > 0 && salePrice < usualPrice;
-  }
 
   let salePrice = productSwatch?.selectedVariation?.salePrice || getSalePrice(item);
   let hasSalePrice = true;
