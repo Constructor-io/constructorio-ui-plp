10037e400e9b5227f5be7d966a2103e505bc3901
Evan Yan <evan.yan@constructor.io>
2025-09-08 11:55:44 -0700

    Update storybook

--- src/stories/components/Filters/Filters.stories.tsx
+++ src/stories/components/Filters/Filters.stories.tsx
@@ -1,9 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import type { Meta, StoryObj } from '@storybook/react';
 import CioPlp from '../../../components/CioPlp';
+import CioPlpGrid from '../../../components/CioPlpGrid';
 import Filters from '../../../components/Filters';
-import mockTransformedFacets from '../../../../spec/local_examples/sampleFacets.json';
-import { PlpFacet } from '../../../types';
 import { DEMO_API_KEY } from '../../../constants';
 import '../../../styles.css';
 
@@ -26,7 +25,14 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 function PrimaryStory({ args }: any) {
-  const [currentUrl, setCurrentUrl] = useState(window.location.href);
+  const [currentUrl, setCurrentUrl] = useState(`${window.location.href}&q=shirt`);
+  // This is used for reactivity, updating this key will force CioPlpGrid to re-render
+  const [gridKey, setGridKey] = useState(1);
+
+  useEffect(() => {
+    setGridKey(gridKey + 1);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [currentUrl]);
 
   return (
     <CioPlp
@@ -37,14 +43,17 @@ function PrimaryStory({ args }: any) {
         },
         getUrl: () => currentUrl,
       }}>
-      <Filters {...args} />
+      <CioPlpGrid {...args} key={gridKey}>
+        {(props) => {
+          const { facets } = props.filters;
+          return <Filters facets={facets} {...props} />;
+        }}
+      </CioPlpGrid>
     </CioPlp>
   );
 }
 
 export const Primary: Story = {
-  render: (args) => <PrimaryStory args={args} />,
-  args: {
-    facets: mockTransformedFacets as Array<PlpFacet>,
-  },
+  args: { facets: [] },
+  render: (args) => <PrimaryStory {...args} />,
 };
