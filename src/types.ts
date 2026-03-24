529e10f57beeccb46693c51b6dd4ab0efa54743e
Enes Kutay SEZEN <eneskutaysezen@gmail.com>
2026-03-24 17:56:29 +0300

    Update types

--- src/types.ts
+++ src/types.ts
@@ -9,6 +9,7 @@ import {
   SearchResponse,
   GetBrowseResultsResponse,
   VariationsMap,
+  VariationsMapResponse,
   FilterExpression,
   FmtOptions,
   Nullable,
@@ -18,6 +19,8 @@ import {
   SearchParameters,
   BrowseRequestType,
   FacetOption as ApiFacetOption,
+  RangeMax,
+  RangeMin,
 } from '@constructor-io/constructorio-client-javascript/lib/types';
 import type { ComponentOverrideProps } from '@constructor-io/constructorio-ui-components';
 
@@ -30,6 +33,7 @@ export {
   ApiFacet,
   ApiFacetOption,
   ApiGroup,
+  VariationsMapResponse,
 };
 
 export interface ApiHierarchicalFacetOption extends ApiFacetOption {
@@ -241,7 +245,7 @@ export interface Item {
   labels: Record<string, unknown>;
   itemName: string;
   variations?: Variation[];
-  variationsMap?: VariationsMap;
+  variationsMap?: VariationsMapResponse;
 
   // Flattened Data Object
   itemId: string;
@@ -376,7 +380,7 @@ export interface PlpFacetOption {
   displayName: string;
   value: string;
   data: Record<string, any>;
-  range?: ['-inf' | number, 'inf' | number];
+  range?: [RangeMin, RangeMax];
   options?: Array<PlpHierarchicalFacetOption>;
 }
 
