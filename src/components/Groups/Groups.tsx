c1b991bf665729550a9a0fda43f6d1f0b27ffd2d
Enes Kutay SEZEN <eneskutaysezen@gmail.com>
2026-05-15 15:39:03 -0500

    Fix merge issue

--- src/components/Groups/Groups.tsx
+++ src/components/Groups/Groups.tsx
@@ -1,56 +1,158 @@
-<RenderPropsWrapper props={renderProps} override={componentOverrides?.reactNode}>
-  <div className='cio-groups-container'>
-    <RenderPropsWrapper props={renderProps} override={componentOverrides?.header?.reactNode}>
-      <button className='cio-filter-header' type='button' onClick={toggleIsCollapsed}>
-        {translatedTitle}
-        <i className={`cio-arrow ${isCollapsed ? 'cio-arrow-up' : 'cio-arrow-down'}`} />
-      </button>
-    </RenderPropsWrapper>
-    <div
-      className={classNames({
-        'cio-collapsible-wrapper': true,
-        'cio-collapsible-is-open': !isCollapsed,
-      })}>
-      <div className='cio-collapsible-inner cio-groups cio-filter-groups-options-list'>
-        <RenderPropsWrapper
-          props={renderProps}
-          override={componentOverrides?.breadcrumbs?.reactNode}>
-          <div className='cio-groups-breadcrumbs'>
-            {breadcrumbs.map((crumb) => (
-              <span key={crumb.path}>
-                <button
-                  className='cio-groups-crumb'
-                  onClick={() => goToGroupFilter(crumb)}
-                  type='button'>
-                  {crumb.breadcrumb}
-                </button>
-                {' > '}
-              </span>
-            ))}
-            <span className='cio-groups-crumb'>
-              {renderProps.currentPage || groups[0]?.displayName || ''}
-            </span>
+import React, { useState } from 'react';
+import classNames from 'classnames';
+import type { IncludeComponentOverrides } from '@constructor-io/constructorio-ui-components';
+import useGroups, { UseGroupProps } from '../../hooks/useGroups';
+import FilterOptionListRow from '../Filters/FilterOptionListRow';
+import type { IncludeRenderProps, GroupsOverrides, GroupsRenderProps } from '../../types';
+import RenderPropsWrapper from '../RenderPropsWrapper/RenderPropsWrapper';
+import { useCioPlpContext } from '../../hooks/useCioPlpContext';
+import { translate } from '../../utils/helpers';
+
+/**
+ * Props for the Groups component
+ */
+export interface GroupsProps extends UseGroupProps, IncludeComponentOverrides<GroupsOverrides> {
+  /**
+   * Initial collapsed state of the groups filter panel
+   * @default false
+   */
+  isCollapsed?: boolean;
+  /**
+   * Title displayed in the groups filter header
+   * @default Categories
+   */
+  title?: string;
+  /**
+   * Whether to hide the entire groups component
+   * @default false
+   */
+  hideGroups?: boolean;
+}
+
+export type GroupsWithRenderProps = IncludeRenderProps<GroupsProps, ReturnType<typeof useGroups>>;
+
+export default function Groups(props: GroupsWithRenderProps) {
+  const {
+    isCollapsed: isCollapsedDefault = false,
+    title,
+    children,
+    groups,
+    hideGroups,
+    componentOverrides: componentOverridesProp,
+  } = props;
+  const context = useCioPlpContext();
+  const componentOverrides = componentOverridesProp ?? context?.componentOverrides?.groups;
+  const translations = context?.translations;
+  const translatedTitle = title || translate('Categories', translations);
+  const useGroupsReturn = useGroups(props);
+  const {
+    optionsToRender,
+    breadcrumbs,
+    initialNumOptions,
+    selectedGroupId,
+    onOptionSelect,
+    goToGroupFilter,
+    isShowAll,
+    setIsShowAll,
+  } = useGroupsReturn;
+
+  const [isCollapsed, setIsCollapsed] = useState(isCollapsedDefault);
+
+  const toggleIsCollapsed = () => setIsCollapsed((prev) => !prev);
+
+  if (hideGroups) return null;
+
+  if (breadcrumbs.length === 0 && optionsToRender.length === 0) return null;
+
+  const renderProps: GroupsRenderProps = {
+    groups,
+    breadcrumbs,
+    currentPage: useGroupsReturn.currentPage,
+    isCollapsed,
+    toggleIsCollapsed,
+    optionsToRender,
+    isShowAll,
+    setIsShowAll,
+    selectedGroupId,
+    onOptionSelect,
+    goToGroupFilter,
+  };
+
+  return (
+    <>
+      {typeof children === 'function' ? (
+        children(useGroupsReturn)
+      ) : (
+        <RenderPropsWrapper props={renderProps} override={componentOverrides?.reactNode}>
+          <div className='cio-groups-container'>
+            <RenderPropsWrapper
+              props={renderProps}
+              override={componentOverrides?.header?.reactNode}>
+              <button className='cio-filter-header' type='button' onClick={toggleIsCollapsed}>
+                {translatedTitle}
+                <i className={`cio-arrow ${isCollapsed ? 'cio-arrow-up' : 'cio-arrow-down'}`} />
+              </button>
+            </RenderPropsWrapper>
+            <div
+              className={classNames({
+                'cio-collapsible-wrapper': true,
+                'cio-collapsible-is-open': !isCollapsed,
+              })}>
+              <div className='cio-collapsible-inner cio-groups cio-filter-groups-options-list'>
+                <RenderPropsWrapper
+                  props={renderProps}
+                  override={componentOverrides?.breadcrumbs?.reactNode}>
+                  <div className='cio-groups-breadcrumbs'>
+                    {breadcrumbs.map((crumb) => (
+                      <span key={crumb.path}>
+                        <button
+                          className='cio-groups-crumb'
+                          onClick={() => goToGroupFilter(crumb)}
+                          type='button'>
+                          {crumb.breadcrumb}
+                        </button>
+                        {' > '}
+                      </span>
+                    ))}
+                    <span className='cio-groups-crumb'>
+                      {renderProps.currentPage || groups[0]?.displayName || ''}
+                    </span>
+                  </div>
+                </RenderPropsWrapper>
+                <RenderPropsWrapper
+                  props={renderProps}
+                  override={componentOverrides?.optionsList?.reactNode}>
+                  <ul>
+                    {optionsToRender.map((option) => (
+                      <FilterOptionListRow
+                        showCheckbox={false}
+                        key={option.groupId}
+                        id={option.groupId}
+                        optionValue={option.groupId}
+                        displayValue={option.displayName}
+                        displayCountValue={option.count.toString()}
+                        isChecked={selectedGroupId === option.groupId}
+                        onChange={onOptionSelect}
+                      />
+                    ))}
+
+                    {initialNumOptions < useGroupsReturn.groupOptions.length && (
+                      <button
+                        type='button'
+                        className='cio-see-all'
+                        onClick={() => setIsShowAll((prevIsShowAll) => !prevIsShowAll)}>
+                        {isShowAll
+                          ? translate('Show Less', translations)
+                          : translate('Show All', translations)}
+                      </button>
+                    )}
+                  </ul>
+                </RenderPropsWrapper>
+              </div>
+            </div>
           </div>
         </RenderPropsWrapper>
-        <RenderPropsWrapper
-          props={renderProps}
-          override={componentOverrides?.optionsList?.reactNode}>
-          <ul>
-            {optionsToRender.map((option) => (
-              <FilterOptionListRow
-                showCheckbox={false}
-                key={option.groupId}
-                id={option.groupId}
-                optionValue={option.groupId}
-                displayValue={option.displayName}
-                displayCountValue={option.count.toString()}
-                isChecked={selectedGroupId === option.groupId}
-                onChange={onOptionSelect}
-              />
-            ))}
-          </ul>
-        </RenderPropsWrapper>
-      </div>
-    </div>
-  </div>
-</RenderPropsWrapper>;
+      )}
+    </>
+  );
+}
